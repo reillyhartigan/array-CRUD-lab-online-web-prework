0d5cd83d40185b0d0622fbcbbf11cefb0d17e008
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-03-31 22:37:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-colors = [pink, white, green, red, black, gold]
+colors = [pink, white, green, red]
 end
 
 def add_element_to_end_of_array(array, element)
